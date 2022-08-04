a958963ba304ab1760f984b1148e3f6bf8bfa97e
Julieta Tatiana Castillo <julietacastillo326@gmail.com>
2022-08-03 23:21:47 -0300

    Actualizando funcionabilidad

--- script/script.js
+++ script/script.js
@@ -5,17 +5,31 @@ btnPedido.addEventListener('click', ()=>{
 })
 
 let pedidos = []; // Array de pedidos
+let clientes = [];
+let numeroCliente = 0;
 const formPedido = document.getElementById("frmPedido"); 
 
 formPedido.addEventListener('submit', (event) => { //Evento del formulario
     event.preventDefault(); //Le sacamos el default
-    /*obtenemos los datos del formulario*/
+    /*obtenemos los datos del Pedido*/
     let seleccioPedido = document.getElementById("seleccionPedido").value;
     let sabor = document.getElementById("seleccionSabor").value;
     let unidades = document.getElementById("introducirCantidad").value;
     let mensaje = document.getElementById("mensaje").value;
 
-    //llamamos al constructor de la clase
+    /*obtenemos los datos del cliente*/
+    let nom = document.getElementById("introducirName").value;
+    let ape = document.getElementById("introducirApellido").value;
+    let mail = document.getElementById("introducirMail").value;
+    let tel = document.getElementById("introducirTelefono").value;
+
+    //llamamos al constructor de clase Cliente
+    const cliente = new Cliente(nom, ape, mail, tel);
+    cliente.visualizarCliente();
+    clientes.push(); //añadimos el cliente al array
+    console.log(clientes);
+
+    //llamamos al constructor de la clase Pedido
     const pedido = new Pedido(seleccioPedido, sabor, unidades, mensaje); 
     pedido.mostrarPedido()
 
@@ -23,6 +37,21 @@ formPedido.addEventListener('submit', (event) => { //Evento del formulario
     console.log(pedidos); //Visualizamos el array por consola
     formPedido.reset(); //reseteamos el formulario
 });
+
+class Cliente{
+    constructor(nombre, apellido, mail, telefono){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.telefono = telefono;
+    }
+    visualizarCliente(){
+        numeroCliente += 1;
+        console.log("Cliente *********************************** N°"+ numeroCliente + "\nDatos\n"+
+                    "Nombre: " + this.nombre + " Apellido: " + this.apellido + "\n" + 
+                    "Contacto\n" +"Mail: " + this.mail + " telefono: " + this.telefono);
+    }
+}
 
 class Pedido{
     constructor(seleccion, sabor, unidades, mensaje){
@@ -32,6 +61,7 @@ class Pedido{
         this.mensaje = mensaje;
     }
     mostrarPedido(){
-        console.log("Se registro el siguiente pedido\nSe pido " + this.unidadPedido + " unidad(es) de " + this.seleccionPedido + " con sabor " + this.saborPedido + "\nAclaración: " + this.mensaje);
+        console.log("Cliente *********************************** N°"+ numeroCliente);
+        console.log("Se pidio " + this.unidadPedido + " de " + this.seleccionPedido + " con sabor " + this.saborPedido + "\nAclaraciones\n" + this.mensaje);
     }
 }
